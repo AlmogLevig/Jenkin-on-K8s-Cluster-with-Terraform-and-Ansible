677d82ea910e8daf819f9a2bd8d7f5a4f128fa8d
almog.levi <almog.levig@gmail.com>
2022-10-22 13:05:51 +0300

    Upload config_file.tf & template files folder

---
+++ Terraform/config_files.tf
@@ -0,0 +1,25 @@
+resource "local_file" "AnsibleInventory" {
+ content = templatefile("./template_files/inventory.tmpl",
+    {
+        master = aws_instance.master.public_ip,
+        agents = flatten([ for i in aws_instance.agents[*].public_ip : i]),
+        ansible_user = var.ansible_user
+    })
+    filename = "../Ansible/hosts"
+    depends_on = [ 
+        aws_volume_attachment.ebsAttach
+    ]
+}
+
+resource "local_file" "ansible_config" {
+ content = templatefile("./template_files/ansible_config.tmpl",
+    {
+        key_path = var.key_path 
+        key_name = var.key_name
+        ansible_user = var.ansible_user
+    })
+    filename = "../Ansible/ansible.cfg"
+    depends_on = [ 
+            local_file.AnsibleInventory
+        ]
+}
